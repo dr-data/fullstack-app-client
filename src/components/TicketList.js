95eda6165d580246a076044322c009fbf9d98d20
Astrid88 <giu.munari@gmail.com>
2019-07-18 19:58:08 +0200

    add styles for each ticket

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
-
+import'./TicketList.css'
 export default function TicketList (props) {
+    
     return (
         <div className='TicketList'>
             <ul>
             {props.tickets.map(ticket =>
-                <li key={ticket.id}>
-                    <Link to={`/event/${ticket.eventId}/ticket/${ticket.id}`}>{ticket.price} EUR {ticket.description}</Link>
+                <li key={ticket.ticket.id} className={props.colorAction(ticket.risk)}>
+                    <Link to={`/event/${ticket.ticket.eventId}/ticket/${ticket.ticket.id}`}>{ticket.ticket.price} EUR {ticket.ticket.description}</Link>
                 </li>
                 )}
             </ul>
